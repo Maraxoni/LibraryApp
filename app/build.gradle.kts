9e5813fa110b2d62b83e04d79cee91a2231129a6
Maraxoni <maraxoni16@gmail.com>
2023-12-21 12:04:10 +0100

    chyba działa

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.libraryapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.libraryapp"
@@ -35,6 +35,17 @@ android {
 }
 
 dependencies {
+
+    implementation("androidx.room:room-runtime:2.6.1")
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
+    androidTestImplementation("androidx.room:room-testing:2.6.1")
+
+    implementation("androidx.lifecycle:lifecycle-common-java8:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation("com.google.android.material:material:1.11.0")
+
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
--- app/src/main/java/com/example/libraryapp/MainActivity.java
+++ app/src/main/java/com/example/libraryapp/MainActivity.java
@@ -8,41 +8,61 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 
-import androidx.core.view.WindowCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-import com.example.libraryapp.databinding.ActivityMainBinding;
-
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+    private BookViewModel bookViewModel;
+    public static final int NEW_BOOK_ACTIVITY_REQUEST_CODE = 1;
+    public static final int EDIT_BOOK_ACTIVITY_REQUEST_CODE = 2;
+    private Book editedBook = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        final BookAdapter adapter = new BookAdapter();
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        setSupportActionBar(binding.toolbar);
+        bookViewModel = ViewModelProviders.of(this).get(BookViewModel.class);
+        bookViewModel.findAll().observe(this, new Observer<List<Book>>() {
+            @Override
+            public void onChanged(@Nullable final List<Book> books) {
+                adapter.setBooks(books);
+            }
+        });
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton addBookButton = findViewById(R.id.add_button);
+        addBookButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, EditBookActivity.class);
+                startActivityForResult(intent, NEW_BOOK_ACTIVITY_REQUEST_CODE);
             }
         });
     }
@@ -70,9 +90,102 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == NEW_BOOK_ACTIVITY_REQUEST_CODE) {
+                Book book = new Book(data.getStringExtra(EditBookActivity.EXTRA_EDIT_BOOK_TITLE),
+                        data.getStringExtra(EditBookActivity.EXTRA_EDIT_BOOK_AUTHOR));
+                bookViewModel.insert(book);
+                Snackbar.make(findViewById(R.id.coordinator_layout),
+                                getString(R.string.book_added),
+                                Snackbar.LENGTH_LONG)
+                        .show();
+            }
+            else if (requestCode == EDIT_BOOK_ACTIVITY_REQUEST_CODE) {
+                editedBook.setTitle(data.getStringExtra(EditBookActivity.EXTRA_EDIT_BOOK_TITLE));
+                editedBook.setAuthor(data.getStringExtra(EditBookActivity.EXTRA_EDIT_BOOK_AUTHOR));
+                bookViewModel.update(editedBook);
+                editedBook = null;
+                Snackbar.make(findViewById(R.id.coordinator_layout),
+                                getString(R.string.book_edited),
+                                Snackbar.LENGTH_LONG)
+                        .show();
+            }
+        }
+        else
+            Snackbar.make(findViewById(R.id.coordinator_layout),
+                            getString(R.string.empty_not_saved),
+                            Snackbar.LENGTH_LONG)
+                    .show();
+    }
+
+    private class BookHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
+        private TextView bookTitleTextView;
+        private TextView bookAuthorTextView;
+        private Book book;
+
+        public BookHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.book_list_item, parent, false));
+            itemView.setOnClickListener(this);
+            itemView.setOnLongClickListener(this);
+
+            bookTitleTextView = itemView.findViewById(R.id.book_title);
+            bookAuthorTextView = itemView.findViewById(R.id.book_author);
+        }
+
+        public void bind(Book book) {
+            this.book = book;
+            bookTitleTextView.setText(book.getTitle());
+            bookAuthorTextView.setText(book.getAuthor());
+        }
+
+        @Override
+        public void onClick(View v) {
+            MainActivity.this.editedBook = this.book;
+            Intent intent = new Intent(MainActivity.this, EditBookActivity.class);
+            intent.putExtra(EditBookActivity.EXTRA_EDIT_BOOK_TITLE, book.getTitle());
+            intent.putExtra(EditBookActivity.EXTRA_EDIT_BOOK_AUTHOR, book.getAuthor());
+            startActivityForResult(intent, EDIT_BOOK_ACTIVITY_REQUEST_CODE);
+        }
+
+        @Override
+        public boolean onLongClick(View v) {
+            MainActivity.this.bookViewModel.delete(this.book);
+            return true;
+        }
+    }
+
+    private class BookAdapter extends RecyclerView.Adapter<BookHolder> {
+        private List<Book> books;
+
+        @NonNull
+        @Override
+        public BookHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new BookHolder(getLayoutInflater(), parent);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull BookHolder holder, int position) {
+            if (books != null) {
+                Book book = books.get(position);
+                holder.bind(book);
+            }
+            else
+                Log.d("MainActivity", "No books");
+        }
+
+        @Override
+        public int getItemCount() {
+            if (books != null)
+                return books.size();
+            return 0;
+        }
+
+        void setBooks(List<Book> books) {
+            this.books = books;
+            notifyDataSetChanged();
+        }
     }
 }
